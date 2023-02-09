4d116cb4a688fddebe105fe67496bf3c3cf3f89c
Daniel Silva Ferreira Bruno <dsfbstudent@yahoo.com.br>
2023-02-09 16:01:33 -0300

    Add ate 'Seletores avancados' na secao 4 em andamento do projeto no seguinte curso da Alura: HTML5 e CSS3 parte 4... (Versao deprecated...)

---
+++ Alura/HTML5 e CSS3 parte 4 avancando no CSS/style-home.css
@@ -0,0 +1,48 @@
+#banner {
+	width:100%;
+}
+
+.principal{
+	background: #CCCCCC;
+	padding: 30px;
+}
+
+.titulo-principal {
+	padding-left: 20px;
+}
+
+.titulo-centralizado {
+	text-align: center
+}
+
+p {
+	text-align: center;
+}
+
+#missao {
+	font-size: 20px
+}
+
+em strong {
+	color: #FF0000;
+}
+
+.itens {
+	font-style: italic
+}
+
+.beneficios {
+	background: #FFFFFF;
+	padding: 20px;
+}
+
+ul {
+	display: inline-block;
+	vertical-align: top;
+	width: 20%;
+	margin-right: 15%;
+}
+
+.imagembeneficios {
+	width: 50%;
+}
