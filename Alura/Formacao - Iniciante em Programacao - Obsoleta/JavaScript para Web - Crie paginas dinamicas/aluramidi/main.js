bc5943c401e77707df3abc7a3598b05fbc8e50b8
dsfb <dsfbstudent@yahoo.com.br>
2023-02-14 11:45:10 -0300

    Add todas as aulas ate o final do projeto no seguinte curso da Alura: JavaScript para Web - Crie paginas dinamicas... (Versao deprecated...)

--- Alura/Formacao - Iniciante em Programacao - Obsoleta/JavaScript para Web - Crie paginas dinamicas/aluramidi/main.js
+++ Alura/Formacao - Iniciante em Programacao - Obsoleta/JavaScript para Web - Crie paginas dinamicas/aluramidi/main.js
@@ -1,5 +1,12 @@
-function tocaSom(idElementoAudio) {
-	document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+	const elemento = document.querySelector(seletorAudio);
+
+	if (elemento && elemento.localName === 'audio') {
+		elemento.play();
+	} else {
+		console.log('Elemento não encontrado ou seletor inválido!!!');
+	}
+
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -17,8 +24,13 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 		tocaSom(idAudio);
 	};
 
-	tecla.onkeydown = function () {
-		tecla.classList.add('ativa');
+	tecla.onkeydown = function (evento) {
+
+		if (evento.code === 'Space' ||
+			evento.code === 'Enter') {
+			tecla.classList.add('ativa');
+		}
+
 	};
 
 	tecla.onkeyup = function () {
